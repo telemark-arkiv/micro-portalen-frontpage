c54fdac1676b245761fbcb596a3340e8f4d8e45f
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-06-17 11:59:39 +0200

    Implements /shortcuts

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ module.exports = async (request, response) => {
     const readme = readFileSync('./README.md', 'utf-8')
     const html = marked(readme)
     send(response, 200, html)
+  } else if (pathname === '/shortcuts') {
+    const results = await axios.get(config.shortcutsUrl)
+    const shortcuts = results.data.map(shortcut => generateCard(shortcut))
+    const index = readFileSync('lib/data/index.html', 'utf-8')
+    const html = index.replace('{{data}}', shortcuts.join(''))
+    send(response, 200, html)
   } else {
     const [front, api] = await Promise.all([urlStatus(config.webUrl), urlStatus(config.apiUrl)])
     if (front === 200 && api === 200) {
